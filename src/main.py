bf0ff45fee2836b3e8b075e2a969c0125a98c3ea
Bitha17 <16521076@mahasiswa.itb.ac.id>
2023-02-28 15:02:52 +0700

    feat: validation & runtime

--- src/main.py
+++ src/main.py
@@ -2,17 +2,31 @@ import randomizer as r
 import distance as d
 import plot as p
 import sort as s
+import time
 
 n = int(input("Please input the number of points: "))
+while n < 2:
+    print("A minimum number of 2 points is needed!")
+    n = int(input("Please input the number of points: "))
+
 dim = int(input("Please input the dimension: "))
+while n < 1:
+    print("The smallest dimension possible is 1.")
+    dim = int(input("Please input the dimension: "))
 
 points = r.randomizer(n,dim)
 print("Your random points are: ")
 for i in range(n):
     print(points[i])
 
-nearest = d.nearest_points(s.merge_sort(points))
-# print("Your nearest pair of points are: ")
-# print(nearest)
+sorted = s.merge_sort(points)
+start = time.time()
+nearest = d.nearest_points(sorted)
+end = time.time()
+print("Your nearest pair of points are: ", end="")
+print(nearest)
 
-# p.plot(dim,points,nearest)
+print("Runtime: ", end="")
+print(end-start)
+
+p.plot(dim,points,nearest)
