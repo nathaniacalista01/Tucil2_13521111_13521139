29fe9f7bd466c607fd4b5247b1ed059795f5a0ab
Bitha17 <16521076@mahasiswa.itb.ac.id>
2023-02-28 16:41:28 +0700

    fix: input

--- src/input.py
+++ src/input.py
@@ -5,16 +5,16 @@ def welcomeMsg():
 
 def inputUser():
     while(True):
-        titik = int(input("Masukkan jumlah titik yang diinginkan : "))
+        titik = int(input("Please input the number of points: "))
         if(titik > 1):
             break
         else :
-            print("Masukkan anda tidak valid, silakan coba lagi!")
+            print("Input not valid, a minimum number of 2 points is needed, please reenter your input!")
 
     while(True):
-        dimensi = int(input("Masukkan banyaknya dimensi titik yang anda inginkan : "))
+        dimensi = int(input("Please input the dimension: "))
         if(dimensi > 0):
             break
         else :
-            print("Masukkan anda tidak valid")
+            print("Input not valid, a minimum of 1 dimension is needed, please reenter your input!")
     return titik,dimensi
